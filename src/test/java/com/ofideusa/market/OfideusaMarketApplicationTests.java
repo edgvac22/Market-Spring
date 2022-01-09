fb9add12370ca02b4efef8d493e53413f8030bba
edgvac2298 <edgardovac2298@gmail.com>
2022-01-09 15:11:48 -0500

    Market using spring

---
+++ src/test/java/com/ofideusa/market/OfideusaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ofideusa.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfideusaMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
